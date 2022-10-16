f16c81b0c7858690df5630b093a0cd02bdab13a0
Nima Zahadat, Ph.D <30416653+captainnima@users.noreply.github.com>
2022-10-16 13:05:05 -0400

    Add files via upload

---
+++ 040 AdventureWorksLT 2019 Top Queries.sql
@@ -0,0 +1,11 @@
+select top 2 *
+from SalesLT.ProductModel
+order by ProductModelID desc
+
+select top 2 *
+from SalesLT.ProductDescription
+order by ProductDescriptionID desc
+
+select top 2 *
+from SalesLT.ProductModelProductDescription
+order by ProductModelID desc
